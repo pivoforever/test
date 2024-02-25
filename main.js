3dabbf0fa665f8482954d9e5de676671119bb4c5
Vit <pivonever@gmail.com>
2024-02-25 15:23:04 +0500

    First Commit

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ $(document).ready(function() {
     'use strict';
     paper.install(window);
     paper.setup(document.getElementById('mainCanvas'));
-    var c=Shape.Circle(200,200,50);
-    c.fillColor='red';
-    paper.view.drow();
+    var c;
+    for (var x=25; x<400; x+=50) {
+        for (var y=25; y<400; y+=50) {
+            c=Shape.Circle(x,y,20);
+            c.fillColor='green'
+        }
+    }
     console.log('main.js loaded');
 });
