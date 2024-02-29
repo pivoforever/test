3f6be1ce0e2ad48eec7163ce66241e3cb28f0845
Vit <pivonever@gmail.com>
2024-02-29 14:25:57 +0500

    Установил babel и отредактировал файл gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 const gulp = require("gulp");
 // Зависимости Gulp будут здесь
+const babel = require("gulp-babel");
+
 gulp.task("default", function () {
   // Задачи Gulp будут здесь
+  // Исходный код для Node
+  gulp.src("es6/**/*.js").pipe(babel()).pipe(gulp.dest("dist"));
+  // исходный код для браузера
+  gulp.src("public/es6/**/*.js").pipe(babel()).pipe(gulp.dest("public/dist"));
 });
